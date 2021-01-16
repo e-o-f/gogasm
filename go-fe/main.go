cdcf76bf85d8f6acdd97b28f688a3f8c299d44f8
Gerald <me@jacobford.co.uk>
2021-01-16 13:35:40 +0000

    feat: go-fe playing around

--- go-fe/main.go
+++ go-fe/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "syscall/js"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
+  c := make(chan struct{}, 0)
 	alert := js.Global().Get("alert")
 	alert.Invoke("Hello wasm!")
+
+  ws := js.Global().Get("WebSocket").New("ws://localhost:5001")
+
+  ws.Call("addEventListener", "open", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+    ws.Call("send", "message testing") 
+    fmt.Printf("sent")
+    return nil
+  }))
+  <-c
 }
