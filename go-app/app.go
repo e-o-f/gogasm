0676874f762db014357043d30226c6efe78b7657
Gerald <me@jacobford.co.uk>
2021-01-17 23:52:26 +0000

    feat: go-fe form context

--- go-app/app.go
+++ go-app/app.go
@@ -1,11 +1,15 @@
 // +build wasm
+
 package main
 
 import "github.com/maxence-charriere/go-app/v7/pkg/app"
 
+var formContext map[string]string = make(map[string]string)
+
 type input struct {
 	app.Compo
 	Value string
+	Name string
 }
 
 func (h *input) Render() app.UI {
@@ -22,10 +26,37 @@ func (h *input) Render() app.UI {
 
 func (h *input) OnInputChange(ctx app.Context, e app.Event) {
 	h.Value = ctx.JSSrc.Get("value").String()
+	formContext[h.Name] = h.Value
 	h.Update()
 }
 
+type button struct {
+	app.Compo
+}
+
+func (h *button) Render() app.UI {
+	return app.Button().Body(app.Text("Submit")).OnClick(h.OnButtonClick)
+}
+
+func (h *button) OnButtonClick(ctx app.Context, e app.Event) {
+	for k, v := range formContext {
+		println(k)
+		println(v)
+	}
+}
+
+type container struct {
+	app.Compo
+}
+
+func (h *container) Render() app.UI {
+	return app.Div().Body(
+		&input{ Name: "fname" },
+		&button{},
+	)
+}
+
 func main() {
-	app.Route("/", &input{})
-	app.Run()                
+	app.Route("/", &container{})
+	app.Run()            
 }
